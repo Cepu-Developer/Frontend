d45ae3ebc3bedcf18bb005574df929dc24983853
Muhammad Ariansyah <115616443+AriiAnn@users.noreply.github.com>
2023-12-06 21:56:06 +0700

    artikel

--- FrontendReact+Vite/src/components/Homepage/CardArtikel.jsx
+++ FrontendReact+Vite/src/components/Homepage/CardArtikel.jsx
@@ -1,25 +1,25 @@
-import { Col } from 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
+import { Col } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
 import { GoArrowUpRight } from "react-icons/go";
 
 const CardArtikel = () => {
-    return (
-        <Col className='mb-4 col-lg-3 col-12'>
-            <Card className='rounded-4 border-0'>
-                <Card.Img className='border-0' height={'200px'} variant="top" src="https://source.unsplash.com/random" />
-                <Card.Body>
-                    <Card.Link href='' className='text-success fw-bold d-flex justify-content-between align-items-center'>
-                        <span>Selengkapnya</span>
-                        <span className='fs-3'><GoArrowUpRight /></span>
-                    </Card.Link>
-                    <Card.Title className='fw-bold my-2'>Berita Terkini</Card.Title>
-                    <Card.Text>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit non dolorem vero expedita iusto ducimus sint quae impedit ut
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </Col>
-    )
-}
+  return (
+    <Col className="mb-4 col-lg-3 col-12">
+      <Card className="rounded-4 border-0">
+        <Card.Img className="border-0" height={"200px"} variant="top" src="https://source.unsplash.com/random" />
+        <Card.Body>
+          <Card.Link href="" className="text-success fw-bold d-flex justify-content-between align-items-center">
+            <span>Selengkapnya</span>
+            <span className="fs-3">
+              <GoArrowUpRight />
+            </span>
+          </Card.Link>
+          <Card.Title className="fw-bold my-2">Berita Terkini</Card.Title>
+          <Card.Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit non dolorem vero expedita iusto ducimus sint quae impedit ut</Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
 
 export default CardArtikel;
