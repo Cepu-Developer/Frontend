d344f665220eb056f38c44b87595ec7841853e57
Muhammad Ariansyah <115616443+AriiAnn@users.noreply.github.com>
2023-12-07 13:32:35 +0700

    Menambahkan artikel fix

--- FrontendReact+Vite/src/components/Homepage/CardArtikel.jsx
+++ FrontendReact+Vite/src/components/Homepage/CardArtikel.jsx
@@ -1,24 +1,61 @@
-import { Col } from "react-bootstrap";
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import { GoArrowUpRight } from "react-icons/go";
+import { Col, Carousel, Row } from "react-bootstrap";
 
 const CardArtikel = () => {
+  const [beritaPolusiUdara, setBeritaPolusiUdara] = useState([]);
+
+  useEffect(() => {
+    // Pemanggilan API dilakukan di sini
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://newsapi.org/v2/everything?q=pollution&apiKey=27924b3220654666ba7a70e6e9d12917");
+        const data = await response.json();
+        setBeritaPolusiUdara(data.articles);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Bagian ini untuk membagi data berita menjadi kelompok 4
+  const groupedBerita = beritaPolusiUdara.reduce((acc, curr, index) => {
+    const groupIndex = Math.floor(index / 4);
+    acc[groupIndex] = acc[groupIndex] || [];
+    acc[groupIndex].push(curr);
+    return acc;
+  }, []);
+
   return (
-    <Col className="mb-4 col-lg-3 col-12">
-      <Card className="rounded-4 border-0">
-        <Card.Img className="border-0" height={"200px"} variant="top" src="https://source.unsplash.com/random" />
-        <Card.Body>
-          <Card.Link href="" className="text-success fw-bold d-flex justify-content-between align-items-center">
-            <span>Selengkapnya</span>
-            <span className="fs-3">
-              <GoArrowUpRight />
-            </span>
-          </Card.Link>
-          <Card.Title className="fw-bold my-2">Berita Terkini</Card.Title>
-          <Card.Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit non dolorem vero expedita iusto ducimus sint quae impedit ut</Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
+    <Carousel className="carousel-horizontal">
+      {groupedBerita.map((group, groupIndex) => (
+        <Carousel.Item key={groupIndex} className="mb-4">
+          <Row>
+            {group.map((berita, index) => (
+              <Col lg={3} xs={12} key={index}>
+                <Card className="rounded-4 border-0">
+                  <Card.Img className="border-0" height={"200px"} variant="top" src={berita.urlToImage} />
+                  <Card.Body>
+                    <Card.Link href={berita.url} className="text-success fw-bold d-flex justify-content-between align-items-center">
+                      <span>Selengkapnya</span>
+                      <span className="fs-3">
+                        <GoArrowUpRight />
+                      </span>
+                    </Card.Link>
+                    <Card.Title className="fw-bold my-2">{berita.title}</Card.Title>
+                    <Card.Text>{berita.description}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Carousel.Item>
+      ))}
+    </Carousel>
   );
 };
 
--- FrontendReact+Vite/src/pages/HomePage.jsx
+++ FrontendReact+Vite/src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
-import { Button, Form, Row } from 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
-import NavbarComponent from '../components/Navbar';
-import CardArtikel from '../components/Homepage/CardArtikel';
-import Footer from '../components/Footer';
-import CardDiskusi from '../components/Homepage/CardDiskusi';
+import { Button, Form, Row, Carousel } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import NavbarComponent from "../components/Navbar";
+import CardArtikel from "../components/Homepage/CardArtikel";
+import Footer from "../components/Footer";
+import CardDiskusi from "../components/Homepage/CardDiskusi";
 
 const HomePage = () => {
   return (
@@ -11,14 +11,16 @@ const HomePage = () => {
       <NavbarComponent />
       <div className="container py-lg-5 pt-0">
         {/* CARD INFORMATION */}
-        <Card className='p-5 border-0 text-center'>
-          <Card.Body className='p-lg-5'>
+        <Card className="p-5 border-0 text-center">
+          <Card.Body className="p-lg-5">
             <h3 className="fw-bold">Cek kualitas udara disini</h3>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente culpa soluta tempora illum exercitationem veniam quo dolor! Non, repellat nisi, nam enim quam quo rerum eveniet facere minima id est!</p>
             <div className="align-items-center">
-              <form action="" method="get" className='d-flex col-lg-8 col-12 mx-auto'>
-                <input type="text" className='form-control me-2' placeholder='Masukkan apa?' name='search' />
-                <button type="submit" className='btn btn-success'>Button</button>
+              <form action="" method="get" className="d-flex col-lg-8 col-12 mx-auto">
+                <input type="text" className="form-control me-2" placeholder="Masukkan apa?" name="search" />
+                <button type="submit" className="btn btn-success">
+                  Button
+                </button>
               </form>
             </div>
           </Card.Body>
@@ -26,25 +28,22 @@ const HomePage = () => {
         {/* CARD INFORMATION */}
 
         <div className="my-5 justify-content-center">
-          <h3 className='fw-bold'>Artikel</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi qui quo iste dignissimos quis obcaecati nulla.</p>
-          <Row className='d-flex m-0'>
+          <h3 className="fw-bold">Artikel</h3>
+          <p>Berita Tentang Polusi udara.</p>
+          <Carousel className="carousel-horizontal">
             <CardArtikel />
-            <CardArtikel />
-            <CardArtikel />
-            <CardArtikel />
-          </Row>
+          </Carousel>
         </div>
 
         <hr />
 
         <div className="my-5 justify-content-center">
-          <h3 className='text-center fw-bold'>Diskusi Populer</h3>
-          <p className='text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <h3 className="text-center fw-bold">Diskusi Populer</h3>
+          <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
           <Row>
             <div className="col-lg-8 col-12 mb-3">
-              <Card className='border-0'>
-                <Card.Body style={{ overflow: 'auto', height:'500px' }}>
+              <Card className="border-0">
+                <Card.Body style={{ overflow: "auto", height: "500px" }}>
                   <CardDiskusi />
                   <CardDiskusi />
                   <CardDiskusi />
@@ -54,14 +53,14 @@ const HomePage = () => {
             </div>
 
             <div className="col-lg-4 col-12">
-              <Card className='border-0'>
+              <Card className="border-0">
                 <Card.Body>
                   <Form>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                       <Form.Label>Buat Diskusi Baru</Form.Label>
-                      <Form.Control as="textarea" rows={5} placeholder='Buat Postingan' />
+                      <Form.Control as="textarea" rows={5} placeholder="Buat Postingan" />
                     </Form.Group>
-                    <Button className='btn btn-success text-white fw-bold w-100'>Posting</Button>
+                    <Button className="btn btn-success text-white fw-bold w-100">Posting</Button>
                   </Form>
                 </Card.Body>
               </Card>
@@ -72,7 +71,7 @@ const HomePage = () => {
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
