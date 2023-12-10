bc09f888c31fbb7883fad6af89d37c16504b539c
Dindaaa <dindarosalin24@gmail.com>
2023-12-10 11:15:46 +0700

    Done Layanan

--- FrontendReact+Vite/src/pages/Layanan.jsx
+++ FrontendReact+Vite/src/pages/Layanan.jsx
@@ -7,8 +7,41 @@ function Layanan() {
       background: '#fcfafa', minHeight: '100vh'
     }}>
       <NavbarComponent />
-      <div className="min-vh-100">
-        Layanan Kesehatan
+      <div className="container-md min-vh-100">
+        <img className="rounded mx-auto d-block m-4" src="./icons/icons8-padlock-1041.svg" alt="" />
+        <div className="text-center rounded mx-auto d-block"
+          style={{
+            width: '32rem'
+          }}>
+          <div className="card-body"></div>
+          <h5>Ups Kamu belum diajak!</h5>
+          <p>Jangan malu buat hubungi kami untuk mendapatkan akun premium dan jelajahi semua fitur sesuka hati bebas iklan</p>
+        </div>
+        <div className="card rounded mx-auto d-block"
+        style={{
+          width: '32rem'
+        }}>
+          <div className="card-body text-center">
+            <h6>Layanan Kesehatan</h6>
+            <p>Fitur ini memungkinkan kamu untuk terhubung dengan fasilitas kesehatan terpercaya untuk membantu kamu menyelesaikan masalah kesehatan</p>
+          </div>
+        </div>
+        <div className="card rounded mx-auto d-block mt-4"
+          style={{
+            width: '32rem'
+          }}>
+          <div className="card-body">
+            <h6>Benefit Langganan</h6>
+            <ol className="list-group mb-2">
+              <li className="list-group-item">Akses penuh seluruh fitur di layanan kesehatan</li>
+              <li className="list-group-item">Menikmati update seluruh fitur udarakita.</li>
+              <li className="list-group-item">Akses penuh aplikasi bebas iklan.</li>
+            </ol>
+            <button className="btn btn-success">
+              Langganan Sekarang
+            </button>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
