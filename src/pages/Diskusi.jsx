6455481b3055e26e4778728318ce3e1c66fb4e3b
gilangriyanto <gilangriyanto27@gmail.com>
2023-12-12 00:18:04 +0700

    menambahkan page diskusi

--- src/pages/Diskusi.jsx
+++ src/pages/Diskusi.jsx
@@ -1,19 +1,64 @@
-import Footer from "../components/Footer"
-import NavbarComponent from "../components/Navbar"
+import { Card, Row, Form, Button } from "react-bootstrap";
+import Footer from "../components/Footer";
+import NavbarComponent from "../components/Navbar";
+import CardDiskusi from "../components/Homepage/CardDiskusi";
 
 function Diskusi() {
   return (
-    <div style={{
-      background: '#fcfafa', minHeight: '100vh'
-    }}>
+    <div style={{ background: "#fcfafa", minHeight: "100vh" }}>
       <NavbarComponent />
       <div className="min-vh-100">
-        Diskusi
+        {/* You can include multiple instances of CardDiskusi here */}
+        <div className="my-5 justify-content-center">
+          <h3 className="text-center fw-bold">Diskusi Populer</h3>
+          <p className="text-center">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </p>
+          <Row>
+            {/* Kartu Diskusi */}
+            <div className="col-lg-8 col-12 mb-3">
+              <Card className="border-0">
+                <Card.Body style={{ overflow: "auto", height: "500px" }}>
+                  <CardDiskusi />
+                  <CardDiskusi />
+                  <CardDiskusi />
+                  <CardDiskusi />
+                  <CardDiskusi />
+                  <CardDiskusi />
+                </Card.Body>
+              </Card>
+            </div>
+
+            {/* Form untuk Posting */}
+            <div className="col-lg-4 col-12">
+              <Card className="border-0">
+                <Card.Body>
+                  <Form>
+                    <Form.Group
+                      className="mb-3"
+                      controlId="exampleForm.ControlTextarea1"
+                    >
+                      <Form.Label>Buat Diskusi Baru</Form.Label>
+                      <Form.Control
+                        as="textarea"
+                        rows={5}
+                        placeholder="Buat Postingan"
+                      />
+                    </Form.Group>
+                    <Button className="btn btn-success text-white fw-bold w-100">
+                      Posting
+                    </Button>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </div>
+          </Row>
+        </div>
+        {/* <CardDiskusi /> */}
       </div>
       <Footer />
     </div>
-  )
-
+  );
 }
 
-export default Diskusi
+export default Diskusi;
