39e9e5c5263f953c9dce874eca9c7a8c237e31dd
Muhammad Ariansyah <115616443+AriiAnn@users.noreply.github.com>
2023-12-16 09:10:15 +0700

    update artikel

--- src/components/Homepage/CardArtikel.jsx
+++ src/components/Homepage/CardArtikel.jsx
@@ -1,28 +1,46 @@
 /* eslint-disable no-unused-vars */
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import { GoArrowUpRight } from "react-icons/go";
 import { Col, Carousel, Row } from "react-bootstrap";
 
 const CardArtikel = () => {
   const [beritaPolusiUdara, setBeritaPolusiUdara] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Pemanggilan API dilakukan di sini
     const fetchData = async () => {
       try {
-        const response = await fetch("https://newsapi.org/v2/everything?q=pollution&apiKey=27924b3220654666ba7a70e6e9d12917");
+        const response = await fetch("https://657d0207853beeefdb9a2ecb.mockapi.io/news");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
         const data = await response.json();
-        setBeritaPolusiUdara(data.articles);
+        if (data) {
+          setBeritaPolusiUdara(data);
+        } else {
+          console.error("Data format is not as expected:", data);
+        }
       } catch (error) {
         console.error("Error fetching data:", error);
+        setError(error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
 
-  // Bagian ini untuk membagi data berita menjadi kelompok 4
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
   const groupedBerita = beritaPolusiUdara.reduce((acc, curr, index) => {
     const groupIndex = Math.floor(index / 4);
     acc[groupIndex] = acc[groupIndex] || [];
@@ -38,7 +56,7 @@ const CardArtikel = () => {
             {group.map((berita, index) => (
               <Col lg={3} xs={12} key={index}>
                 <Card className="rounded-4 border-0">
-                  <Card.Img className="border-0" height={"200px"} variant="top" src={berita.urlToImage} />
+                  <Card.Img className="border-0 img-fluid" height={"200px"} variant="top" src={berita.urlToImage} />
                   <Card.Body>
                     <Card.Link href={berita.url} className="text-success fw-bold d-flex justify-content-between align-items-center">
                       <span>Selengkapnya</span>
